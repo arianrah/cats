b9db270726a3680bf18046da8f740c3bc3fb05c2
Arian Rahimi <arianrahimi@icloud.com>
2019-09-25 00:51:46 +0000

    cat finalized

---
+++ asyncBreeds.js
@@ -0,0 +1,17 @@
+// asyncBreeds.js
+const catBreeds = {
+  'Balinese': "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.",
+  'Bombay': "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder."
+}
+
+const fs = require('fs');
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log('Callback: I have the data!')
+    if (!error) callback(data); 
+    if (error) callback(undefined);
+  });
+}
+
+module.exports = breedDetailsFromFile;
